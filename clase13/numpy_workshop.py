8e354c1c0551841f83fb7d2a14dc91bcc7c13295
Cesar Augusto Diaz Arriaga <cesar.a.diaz@gmail.com>
2021-05-27 22:14:28 -0500

    Mejoras en funciones de clase 13

--- clase13/numpy_workshop.py
+++ clase13/numpy_workshop.py
@@ -1,29 +1,31 @@
 import numpy as np
 
 # Array de 1 dimension
-a = np.array([4, 7, 5])
+a = np.array([4, 7, 5]); print(a, "\n")
 print(a.shape, "\n")
 a[1] = 2
 print(a, "\n")
 
 # Matriz (2 dimensiones)
-b = np.array([[1,2,3],[4,5,6]])
+b = np.array([[1,2,3],[4,5,6]]); print(b, "\n")
 print(b.shape, "\n") 
-print(b, "\n")
 print(b[0, 0], b[0, 1], b[1, 0], "\n")
 
 # Generar una matrices básicas
 print(np.zeros((4, 3)), "\n")
 print(np.ones((3, 4)), "\n")
 print(np.identity(5), "\n")
+print(np.eye(4,3), "\n")
 
 # Generar valores 
 print(np.arange(1, 30, 2), "\n")
 print(np.arange(30, 1, -2, dtype=float), "\n")
 print(np.arange(1, 30, 2).reshape(3, 5), "\n")
+print(np.arange(1, 32, 2).reshape(4, 4), "\n")
 
 print(np.linspace(1, 10, 10), "\n")
 print(np.linspace(2, 4, 10), "\n")
+print(np.linspace(-2, 4, 30), "\n")
 
 print(np.random.random((3,3)), "\n")
 
@@ -35,13 +37,13 @@ b=np.array([2, 4, 0, 1, 2])
 
 print(a - b, "\n")
 print(b**2, "\n")
-print(2*b, "\n")
-print(a*b, "\n")
+print(2 * b, "\n")
+print(a * b, "\n")
 
 print(b > 2, "\n")
+print(b > a, "\n")
 
 print(a.dot(b), "\n") # Producto Punto
-# print(a.cross(b), "\n") # Producto Cruz
 
 # Funciones
 print(np.sin(a), "\n")
@@ -57,6 +59,15 @@ x=np.array([[1,1],[0,1]]); print(x, "\n")
 y=np.array([[2,0],[3,4]]); print(y, "\n")
 print(x * y, "\n")
 print(x.dot(y), "\n")
+
+print(np.cross(x, y), "\n")   # Producto Cruz
+
+print(np.linalg.inv(x), "\n") # Matriz inversa x
+print(np.linalg.inv(y), "\n") # Matriz inversa y
+print(x.dot(np.linalg.inv(x)), "\n") #  El producto punto de X con su inversa genera la Identidad
+
+print(y.diagonal(), "\n")            # Diagonal de la matriz
+print(np.fliplr(y).diagonal(), "\n") # Diagonal invertida
 
 print(x.sum(), "\n")
 print(x.sum(axis = 0), "\n") # columnas
--- clase13/pandas_workshop.py
+++ clase13/pandas_workshop.py
@@ -9,18 +9,16 @@ import numpy as np
 # Series(data=lista, index=indices, dtype=tipo) : Devuelve un objeto de tipo Series
 #     con los datos de la lista lista, las filas especificados en la lista indices
 #     y el tipo de datos indicado en tipo. 
-s = pd.Series(['Matemáticas', 'Historia', 'Economía', 'Programación', 'Inglés'], dtype='string')
-print(s, "\n")
+s = pd.Series(['Matemáticas', 'Historia', 'Economía', 'Programación', 'Inglés'], dtype='string'); print(s, "\n")
 
-s = pd.Series({'Matemáticas': 6.0,  'Economía': 4.5, 'Programación': 8.5})
-print(s, "\n")
+s = pd.Series({'Matemáticas': 6.0,  'Economía': 4.5, 'Programación': 8.5}); print(s, "\n")
 print(s[1:3], "\n")
 print(s['Economía'], "\n")
 print(s[['Programación', 'Matemáticas']], "\n")
 
 # Filtrar la serie
 print(s > 5, "\n")
-print(s[s > 5])
+print(s[s > 5], "\n")
 
 s = pd.Series([1, 2, 2, 3, 3, 3, 4, 4, 4, 4]); print(s)
 print(s.size)
@@ -37,7 +35,7 @@ print(s.std())
 print(s.describe(), "\n")
 
 # Aplicar una función       
-s = pd.Series(['a', 'b', 'c'])
+s = pd.Series(['a', 'b', 'c']); print(s)
 print(s.apply(str.upper), "\n")
 print(s.apply(lambda x : str(x)+"123"), "\n")
 
@@ -52,13 +50,13 @@ datos = {
     'grado' : ['Economía', 'Medicina', 'Arquitectura', 'Economía'],
     'correo' : ['maria@gmail.com', 'luis@yahoo.es', 'carmen@gmail.com', 'antonio@gmail.com']
 }
-df = pd.DataFrame(datos)
+df = pd.DataFrame(datos);
 print(df, "\n")
 
-df = pd.DataFrame([['María', 18], ['Luis', 22], ['Carmen', 20]], columns=['Nombre', 'Edad'])
+df = pd.DataFrame([['María', 18], ['Luis', 22], ['Carmen', 20]], columns=['Nombre', 'Edad']);
 print(df, "\n")
 
-df = pd.DataFrame(np.random.randn(4, 3), columns=['a', 'b', 'c'])
+df = pd.DataFrame(np.random.randn(4, 3), columns=['a', 'b', 'c']);
 print(df, "\n")
 
 df = pd.read_csv('https://raw.githubusercontent.com/asalber/manual-python/master/datos/colesterol.csv')
@@ -86,7 +84,10 @@ datos = {
     'Programación':[6.5, 4, 9]}
 df = pd.DataFrame(datos)
 print(df, "\n")
+
 df1 = df.melt(id_vars=['nombre', 'edad'], var_name='asignatura', value_name='nota')
 print(df1, "\n")
+
+print(df1.pivot(index=['nombre', 'edad'], columns='asignatura', values='nota'), "\n")
 
 print(df1.pivot(index='nombre', columns='asignatura', values='nota'), "\n")
