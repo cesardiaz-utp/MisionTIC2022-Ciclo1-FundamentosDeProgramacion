375ffbd14c3f1f857b7f7b8a9782118a4910c16a
ALJIQUITA <aljiquita@gmail.com>
2023-01-25 10:25:37 -0500

    sd

--- clase02/eje.py
+++ clase02/eje.py
@@ -1,1 +1,9 @@
-print(range(7))
+peso_producto_en_gramos = 100
+precio_producto = 5.95
+peso_kilo_en_gramos = 1000
+
+precio_correcto = ( peso_kilo_en_gramos / peso_producto_en_gramos ) * precio_producto
+print(precio_correcto) 
+
+precio_error = peso_kilo_en_gramos / peso_producto_en_gramos  * precio_producto
+print(precio_error)
