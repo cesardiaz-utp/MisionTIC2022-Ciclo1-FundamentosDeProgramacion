098b4b77236c88eb9e2ffa311a3d46d5c3b61098
ALJIQUITA <aljiquita@gmail.com>
2023-01-26 15:47:26 -0500

    ds

---
+++ clase03/ejer.py
@@ -0,0 +1,21 @@
+# recibir información en variables con input()
+
+print('Ingresa tu nombre:')
+nombre = input()
+apellido = input('Ingrese su apellido: ')
+
+print(nombre)
+print(type(nombre))
+
+print('Ingresa tu Edad:')
+edad = int(input())
+
+print(edad)
+print(type(edad))
+
+print('Ingresa tu Edad:')
+edad = float(input())
+
+print(edad)
+print(type(edad))
+
